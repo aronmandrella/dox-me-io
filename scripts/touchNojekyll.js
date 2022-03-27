e6ff2f35cbd75aa59c2a0ea7ff7ba331feb1453e
Aron <aron.mandrella@gmail.com>
2022-03-27 19:40:19 +0200

    typo fix

--- scripts/touchNojekyll.js
+++ scripts/touchNojekyll.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 
-// Without this file in /out Next.js app deployed to GitHub pages won't work corrently
+// Without this file in /out Next.js app deployed to GitHub pages won't work currently
 fs.closeSync(fs.openSync("./out/.nojekyll", "w"));
