af293e35a3d120610be43f9bc5f20f5e22a09d2f
Aron <aron.mandrella@gmail.com>
2022-03-27 19:02:42 +0200

    Deploy gh-pages

---
+++ out/_next/static/i7DIU8vvF51vDxogWKPD0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/360297fc7a786196.css","static/chunks/pages/index-1599ecf0d3a8215a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
